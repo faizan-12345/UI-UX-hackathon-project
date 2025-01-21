83cd26b901830c9ffb39feff12ee0563d2346cc3
faizan-12345 <faizanfaisal546@gmail.com>
2025-01-21 09:45:54 +0500

    Fixes img bug

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -23,7 +23,8 @@ export default function Hero() {
         <div className="relative h-[375px] w-full overflow-hidden">
           <div className="absolute inset-0 w-[120%] -left-[10%]">
             <Image
-              src="https://s3-alpha-sig.figma.com/img/b062/f355/fa9b27255272d76e665cce4314b4aedb?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DixWWrqsemtj3S8EoYUwoBDz6ewq6Q0wLdFNqKYZzrQ96nO4PM7p7h87oPMUCBe~arheyKeEZeO0uY9DIFxBcaibJ0IVldW49BZXQLR-1hUdaRKfUrDRmJCc~czCh3Mm8NWD4OieerNtXf75BsB3nsveXCt3KNuu~J1Oy1ydG-9lyYmufZXkhY0unxv0nBkkM7S5g4EpbRJmdY3nIBuW1v3aMA6uI467cvwvTXtKp1m0ZfRs6j--c4U8oMcgnhUgTAwfBQP2fSqmIpfmus9iotvC43YFCbXcRHjjWl4-~VuKQ6-j0GqdB~N41WcAV7ftMMrpKB9dNBSmiDzVOeRA8w__"
+              // src="https://s3-alpha-sig.figma.com/img/b062/f355/fa9b27255272d76e665cce4314b4aedb?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DixWWrqsemtj3S8EoYUwoBDz6ewq6Q0wLdFNqKYZzrQ96nO4PM7p7h87oPMUCBe~arheyKeEZeO0uY9DIFxBcaibJ0IVldW49BZXQLR-1hUdaRKfUrDRmJCc~czCh3Mm8NWD4OieerNtXf75BsB3nsveXCt3KNuu~J1Oy1ydG-9lyYmufZXkhY0unxv0nBkkM7S5g4EpbRJmdY3nIBuW1v3aMA6uI467cvwvTXtKp1m0ZfRs6j--c4U8oMcgnhUgTAwfBQP2fSqmIpfmus9iotvC43YFCbXcRHjjWl4-~VuKQ6-j0GqdB~N41WcAV7ftMMrpKB9dNBSmiDzVOeRA8w__"
+              src="/images/feature.jpg"
               alt="Luxury homeware showcase"
               fill
               className="object-cover object-center"
@@ -37,7 +38,8 @@ export default function Hero() {
       <div className="hidden md:block relative h-[600px]">
         {/* Background Image */}
         <Image
-          src="https://s3-alpha-sig.figma.com/img/b062/f355/fa9b27255272d76e665cce4314b4aedb?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DixWWrqsemtj3S8EoYUwoBDz6ewq6Q0wLdFNqKYZzrQ96nO4PM7p7h87oPMUCBe~arheyKeEZeO0uY9DIFxBcaibJ0IVldW49BZXQLR-1hUdaRKfUrDRmJCc~czCh3Mm8NWD4OieerNtXf75BsB3nsveXCt3KNuu~J1Oy1ydG-9lyYmufZXkhY0unxv0nBkkM7S5g4EpbRJmdY3nIBuW1v3aMA6uI467cvwvTXtKp1m0ZfRs6j--c4U8oMcgnhUgTAwfBQP2fSqmIpfmus9iotvC43YFCbXcRHjjWl4-~VuKQ6-j0GqdB~N41WcAV7ftMMrpKB9dNBSmiDzVOeRA8w__"
+          // src="https://s3-alpha-sig.figma.com/img/b062/f355/fa9b27255272d76e665cce4314b4aedb?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DixWWrqsemtj3S8EoYUwoBDz6ewq6Q0wLdFNqKYZzrQ96nO4PM7p7h87oPMUCBe~arheyKeEZeO0uY9DIFxBcaibJ0IVldW49BZXQLR-1hUdaRKfUrDRmJCc~czCh3Mm8NWD4OieerNtXf75BsB3nsveXCt3KNuu~J1Oy1ydG-9lyYmufZXkhY0unxv0nBkkM7S5g4EpbRJmdY3nIBuW1v3aMA6uI467cvwvTXtKp1m0ZfRs6j--c4U8oMcgnhUgTAwfBQP2fSqmIpfmus9iotvC43YFCbXcRHjjWl4-~VuKQ6-j0GqdB~N41WcAV7ftMMrpKB9dNBSmiDzVOeRA8w__"
+          src="/images/feature.jpg"
           alt="Luxury homeware showcase"
           fill
           className="object-cover object-center"
